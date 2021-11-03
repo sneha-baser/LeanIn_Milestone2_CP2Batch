f80b037cffc4faf79e5eedd9c65714138ed9d2ee
man-15nidhi <85814555+man-15nidhi@users.noreply.github.com>
2021-11-03 16:10:29 +0530

    Add files via upload

---
+++ Next Greater Element.cpp
@@ -0,0 +1,43 @@
+/*Next Greater Element 
+Time Complexity: O(N) 
+Space Complexity : O(N)*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(int arr[], int n)
+{
+    stack<int> s;
+    s.push(arr[0]);
+    for (int i = 1; i < n; i++) 
+    {
+
+        if (s.empty()) {
+            s.push(arr[i]);
+            continue;
+        }
+        while (s.empty() == false 
+               && s.top() < arr[i]) 
+        {
+            cout << s.top() 
+                 << " --> " << arr[i] << endl;
+            s.pop();
+        }
+        s.push(arr[i]);
+    }
+
+     //print -1, if there is no next greater element for remaining element of the stack
+    while (s.empty() == false) {
+        cout << s.top() << " --> " << -1 << endl;
+        s.pop();
+    }
+}
+
+int main()
+{
+    int arr[] = { 3,4,9,11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    print(arr, n);
+    return 0;
+}
+    
